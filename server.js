37665214b2767d50db0e8fecd198796c8408ef34
Mateusz <mateusz.dettlaff0@gmail.com>
2023-05-04 13:11:06 +0300

    changed start script

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const appPort = process.env.PORT | 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, "build");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(appPort, () => {
-  console.log("Server started on port " + appPort);
+app.listen(port, () => {
+  console.log("Server is up!");
 });
