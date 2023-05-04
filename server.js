573ff2608f3f5802e89aca3c4942a9f6b2bffece
Mateusz <mateusz.dettlaff0@gmail.com>
2023-05-04 12:53:09 +0300

    added heroku build

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const appPort = process.env.port | 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(appPort, () => {
+  console.log("Server started on port " + appPort);
+});
