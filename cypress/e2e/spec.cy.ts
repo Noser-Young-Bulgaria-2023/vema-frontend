a9d39557b366d27b97af9cd627c6d9824182da32
Mateusz <mateusz.dettlaff0@gmail.com>
2023-05-05 10:27:22 +0300

    Update spec.cy.ts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,10 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.google.ch/')
-    cy.get('#W0wltc > .QS5gu').click() 
-    cy.get('#APjFqb')
-  })
-})
-
-
-
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://www.google.ch/");
+  });
+});
